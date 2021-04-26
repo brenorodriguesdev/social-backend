6faf899503074bef013eb6d13e64dec3d1777dd3
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-26 17:37:33 -0300

    fix: inviteRepository create

--- infra/inviteRepository.ts
+++ infra/inviteRepository.ts
@@ -4,6 +4,6 @@ import { database } from "../main/config/database";
 
 export class InviteRepositoryPostgres implements InviteRepository {
     async create(invite: Invite): Promise<void> {
-        database.none('insert into user_invite (id_user, id_guest, view) values ($1, $2, $3)', [invite.id_user, invite.id_guest, invite.view);
+        database.none('insert into user_invite (id_user, id_guest, view) values ($1, $2, $3)', [invite.id_user, invite.id_guest, invite.view]);
     }
 }
