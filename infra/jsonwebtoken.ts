9c63beccbff3fa04672f9c871fa224865650301c
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-26 11:32:44 -0300

    feat: add jsonwebtoken

---
+++ infra/jsonwebtoken.ts
@@ -0,0 +1,12 @@
+import { Crypter, Encrypter } from "../data/contracts";
+import * as jwt from 'jsonwebtoken'
+
+export class JsonWebToken implements Crypter, Encrypter {
+    constructor(private readonly secretKey: string) { }
+    async crypt(data: any): Promise<string> {
+        return await jwt.sign(data, this.secretKey)
+    }
+    async encrypt(value: string): Promise<any> {
+        try { return await jwt.verify(value, this.secretKey) } catch { return undefined; }
+    }
+}
