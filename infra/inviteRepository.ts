e7ca6ae4c1e4ff0bbae08b95815efdd7c05b3065
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-26 17:36:58 -0300

    feat: add inviteRepositoryPostgres

---
+++ infra/inviteRepository.ts
@@ -0,0 +1,9 @@
+import { InviteRepository } from "../data/contracts";
+import { Invite } from "../data/models/invite";
+import { database } from "../main/config/database";
+
+export class InviteRepositoryPostgres implements InviteRepository {
+    async create(invite: Invite): Promise<void> {
+        database.none('insert into user_invite (id_user, id_guest, view) values ($1, $2, $3)', [invite.id_user, invite.id_guest, invite.view);
+    }
+}
