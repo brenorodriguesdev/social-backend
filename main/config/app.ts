644eae0345171ae553906854ce969319514d6eeb
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-26 18:55:38 -0300

    feat: setupRoutes

--- main/config/app.ts
+++ main/config/app.ts
@@ -1,9 +1,11 @@
 import cors from 'cors'
 import express from 'express'
+import setupRoutes from './routes'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+setupRoutes(app)
 
 export default app
