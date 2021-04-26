6499921c8436331ccc55d363ec3a2454d1b3faa7
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-26 11:15:52 -0300

    feat: add signIn validation factory

---
+++ main/factories/validations/sign-in-validation-factory.ts
@@ -0,0 +1,14 @@
+import { Validator } from "../../../validation/contracts/validator";
+import { CompositeValidator } from "../../../validation/validator/composite-validator";
+import { EmailValidator } from "../../../validation/validator/email-validator";
+import { RequiredFieldValidator } from "../../../validation/validator/required-field-validator";
+
+export const SignInValidationFactory = (): Validator => {
+    const requiredFields = ['email', 'password']
+    const validators = []
+    for (let requiredField of requiredFields) {
+        validators.push(new RequiredFieldValidator(requiredField))
+    }
+    validators.push(new EmailValidator('email'))
+    return new CompositeValidator(validators)
+}
