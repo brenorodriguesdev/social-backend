7e854a6d8f9b80be94eec0fe62689793891e44ca
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-26 11:08:25 -0300

    feat: add createUserValidation factory method

--- validation/validator/required-field-validator.ts
+++ validation/validator/required-field-validator.ts
@@ -1,7 +1,7 @@
 import { RequiredFieldError } from "../../presentation/errors/required-field-error";
 import { Validator } from "../contracts/validator";
 
-export class RequiredField implements Validator {
+export class RequiredFieldValidator implements Validator {
     constructor(private readonly fieldName: string) { }
     validate(data: any): Error {
         if (!data[this.fieldName]) {
