159231a4f50c8174b58945ddac5400e65d4fdd3d
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-27 10:36:04 -0300

    fix: findAllChat and lastMessage query

--- infra/chat-repository.ts
+++ infra/chat-repository.ts
@@ -11,12 +11,10 @@ export class ChatRepositoryPostgres implements ChatRepository {
     }
     async findAllChat(idUser: number): Promise<Chat[]> {
         const chatList: Chat[] = await database.manyOrNone('select * from chat where id_sender = $1 or id_receiver = $2 order by id desc', [idUser, idUser]);
-        const chatListWithName: Chat[] = []
-        chatList.map(async chat => {
-            const nameSender: string = await database.oneOrNone('select name from user_account where id = $1', [chat.id_sender]);
-            const nameReceiver: string = await database.oneOrNone('select name from user_account where id = $1', [chat.id_receiver]);
-            chatListWithName.push({ ...chat, nameSender, nameReceiver })
-        })
-        return chatListWithName
+        return await Promise.all(chatList.map(async (chat): Promise<Chat> => {
+            chat.nameSender = await database.oneOrNone('select name from user_account where id = $1', [chat.id_sender]);
+            chat.nameReceiver = await database.oneOrNone('select name from user_account where id = $1', [chat.id_receiver]);
+            return chat
+        }))
     }
 }
--- infra/message-repository.ts
+++ infra/message-repository.ts
@@ -8,6 +8,7 @@ export class MessageRepositoryPostgres implements MessageRepository {
     } 
 
     async lastMessage(idChat: number): Promise<Message> {
-        return await database.oneOrNone('select * from message where id_chat = $1 order by id desc', [idChat]);
+        console.log(idChat)
+        return await database.oneOrNone('select * from message where id_chat = $1 order by id desc limit 1', [idChat]);
     }
 }
