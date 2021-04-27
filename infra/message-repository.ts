d72541a62ae091e8168987128d11e7055fc6e1bd
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-27 06:53:29 -0300

    fix: messageRepository create implements corrects values

--- infra/message-repository.ts
+++ infra/message-repository.ts
@@ -4,6 +4,6 @@ import { database } from "../main/config/database";
 
 export class MessageRepositoryPostgres implements MessageRepository {
     async create(message: Message): Promise<void> {
-        await database.none('insert into message (id_chat, message) values ($1, $2)', [message.id_chat, message]);
+        await database.none('insert into message (id_chat, message) values ($1, $2)', [message.id_chat, message.message]);
     } 
 }
