0938b813a8f9d97bed4fa9712910f860774907b0
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-27 00:09:34 -0300

    feat: add chat, message query

--- querys/query.sql
+++ querys/query.sql
@@ -20,3 +20,15 @@ create table friend_list
  id_guest serial NOT NULL,
  FOREIGN KEY (id_user) REFERENCES user_account(id),
  FOREIGN KEY (id_guest) REFERENCES user_account(id));
+
+create table chat
+(id serial primary key, 
+ id_sender serial NOT NULl,
+ id_receiver serial NOT NULL,
+ FOREIGN KEY (id_sender) REFERENCES user_account(id),
+ FOREIGN KEY (id_receiver) REFERENCES user_account(id));
+
+create table message
+(id serial primary key, 
+ id_chat serial NOT NULl,
+ FOREIGN KEY (id_chat) REFERENCES chat(id));
