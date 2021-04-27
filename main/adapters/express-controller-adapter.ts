9023f7e28eea3cc61e8c60f9268bfb3533f6633c
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-27 09:58:29 -0300

    feat: add params on httpRequest

--- main/adapters/express-controller-adapter.ts
+++ main/adapters/express-controller-adapter.ts
@@ -6,6 +6,7 @@ export const adaptRouter = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
       body: req.body,
+      params: req.params
     }
     const httpResponse = await controller.handle(httpRequest)
     if (httpResponse.statusCode !== 200 && httpResponse.statusCode !== 201) {
