62e9ce6be3ab685938c420a9a90680bb48359d12
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-27 00:15:26 -0300

    feat: add sendMessage service

--- data/contracts/chat-repository.ts
+++ data/contracts/chat-repository.ts
@@ -2,4 +2,5 @@ import { Chat } from "../models/chat";
 
 export interface ChatRepository {
     create(chat: Chat): Promise<Chat>
+    findByUsers(idSender: number, idReceiver: number): Promise<Chat>
 }
--- data/services/send-message.ts
+++ data/services/send-message.ts
@@ -1,8 +1,21 @@
 import { SendMessageModel } from "../../domain/models/send-message";
 import { SendMessageUseCase } from "../../domain/useCases/send-message";
+import { ChatRepository } from "../contracts/chat-repository";
+import { MessageRepository } from "../contracts/message-repository";
 
 export class SendMessageService implements SendMessageUseCase {
+    constructor(private readonly chatRepository: ChatRepository, private readonly messageRepository: MessageRepository) { }
     async send(sendMessageModel: SendMessageModel): Promise<void | Error> {
-        
+        let alreadyChat = await this.chatRepository.findByUsers(sendMessageModel.idSender, sendMessageModel.idReceiver)
+        if (!alreadyChat) {
+            alreadyChat = await this.chatRepository.create({
+                id_sender: sendMessageModel.idSender,
+                id_receiver: sendMessageModel.idReceiver
+            })
+        }
+        await this.messageRepository.create({
+            id_chat: alreadyChat.id,
+            message: sendMessageModel.message
+        })
     }
 }
