8aad684590b13b878a36310a7d12a454847ba69d
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 00:42:18 -0300

    feat: add get friend list route

--- main/routes/user.ts
+++ main/routes/user.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { adaptRouter } from "../adapters/express-controller-adapter";
+import { makeGetFriendListController } from "../factories/controllers/get-friend-list-controller-factory";
 import { makeSearchUserController } from "../factories/controllers/search-user-factory";
 import { auth } from "../middlewares/auth";
 
@@ -7,4 +8,5 @@ import { auth } from "../middlewares/auth";
 
 export default (router: Router): void => {
     router.get('/searchUser?:user', auth, adaptRouter(makeSearchUserController()))
+    router.get('/getFriendList?:idUser', auth, adaptRouter(makeGetFriendListController()))
 }
