09087ee87b63091205fc0ed589966083b73b51a9
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 19:41:32 -0300

    feat: add refuse invite route

--- main/factories/controllers/refuse-invite-controller-factory.ts
+++ main/factories/controllers/refuse-invite-controller-factory.ts
@@ -4,7 +4,7 @@ import { InviteRepositoryPostgres } from "../../../infra/invite-repository";
 import { RefuseInviteController } from "../../../presentation/controllers/refuse-invite";
 import { makeRefuseInviteValidation } from "../validations/refuse-invite-validation-factory";
 
-export const makeAcceptInviteController = (): RefuseInviteController => {
+export const makeRefuseInviteController = (): RefuseInviteController => {
     const inviteRepositoryPostgres = new InviteRepositoryPostgres()
     const refuseInviteService = new RefuseInviteService(inviteRepositoryPostgres)
     return new RefuseInviteController(makeRefuseInviteValidation(), refuseInviteService)
--- main/routes/invite.ts
+++ main/routes/invite.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { adaptRouter } from "../adapters/express-controller-adapter";
 import { makeAcceptInviteController } from "../factories/controllers/accept-invite-controller-factory";
 import { makeGetInviteListController } from "../factories/controllers/get-invite-list-controller-factory";
+import { makeRefuseInviteController } from "../factories/controllers/refuse-invite-controller-factory";
 import { makeSendInviteController } from "../factories/controllers/send-invite-controller-factory";
 import { auth } from "../middlewares/auth";
 
 
 export default (router: Router): void => {
     router.post('/sendInvite', auth, adaptRouter(makeSendInviteController()))
+    router.post('/refuseInvite', auth, adaptRouter(makeRefuseInviteController()))
     router.post('/acceptInvite', auth, adaptRouter(makeAcceptInviteController()))
     router.get('/getInviteList', auth, adaptRouter(makeGetInviteListController()))
 }
