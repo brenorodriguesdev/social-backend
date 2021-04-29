a3ce5e9192c3f85e1284126b9ea52cbb4c7477ea
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 14:46:54 -0300

    feat: add getInviteList controller

---
+++ presentation/controllers/get-invite-list.ts
@@ -0,0 +1,17 @@
+import { GetInviteListUseCase } from "../../domain/useCases/get-invite-list";
+import { Controller } from "../contracts/controller";
+import { HttpRequest, HttpResponse } from "../contracts/http";
+import { ok, serverError } from "../contracts/http-helper";
+
+export class GetInviteListController implements Controller {
+    constructor(private readonly getInviteListUseCase: GetInviteListUseCase) { }
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { id } = httpRequest.body
+            const invitesModel = this.getInviteListUseCase.get(id)
+            return ok(invitesModel)
+        } catch (error) {
+            return serverError()
+        }
+    }
+}
