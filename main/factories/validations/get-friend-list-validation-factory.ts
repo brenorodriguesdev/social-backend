fe4a2dd198cccd68bcbe451d1cc9ca12e7f00c31
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 00:40:04 -0300

    feat: add get friend list validation factory

---
+++ main/factories/validations/get-friend-list-validation-factory.ts
@@ -0,0 +1,13 @@
+import { Validator } from "../../../validation/contracts/validator"
+import { CompositeValidator } from "../../../validation/validator/composite-validator"
+import { RequiredFieldValidator } from "../../../validation/validator/required-field-validator"
+
+
+export const makeGetFriendListValidation = (): Validator => {
+    const requiredFields = ['idUser']
+    const validators = []
+    for (let requiredField of requiredFields) {
+        validators.push(new RequiredFieldValidator(requiredField))
+    }
+    return new CompositeValidator(validators)
+}
