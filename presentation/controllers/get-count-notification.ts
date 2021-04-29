b0a0a97bb8ec179146c5fed6e5f33298ebb030e8
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 20:40:06 -0300

    feat: add get count notification controller

---
+++ presentation/controllers/get-count-notification.ts
@@ -0,0 +1,17 @@
+import { GetCountNotificationUseCase } from "../../domain/useCases/get-count-notification";
+import { Controller } from "../contracts/controller";
+import { HttpRequest, HttpResponse } from "../contracts/http";
+import { ok, serverError } from "../contracts/http-helper";
+
+export class GetCountNotification implements Controller {
+    constructor(private readonly getCountNotificationUseCase: GetCountNotificationUseCase) { }
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const { id } = httpRequest.body
+            const count = await this.getCountNotificationUseCase.get(id)
+            return ok(count)
+        } catch (error) {
+            return serverError()
+        }
+    }
+}
