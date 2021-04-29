305f3428aa9a9aa0d97e122e67de7defa52be8b6
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 00:34:36 -0300

    feat: implements findByUser on friendList repository

--- infra/friend-list-repository.ts
+++ infra/friend-list-repository.ts
@@ -1,5 +1,6 @@
 import { FriendListRepository } from "../data/contracts/friend-list-repository";
 import { FriendList } from "../data/models/friend-list";
+import { User } from "../data/models/user";
 import { database } from "../main/config/database";
 
 export class FriendListRepositoryPostgres implements FriendListRepository {
@@ -8,6 +9,9 @@ export class FriendListRepositoryPostgres implements FriendListRepository {
     } 
     async findByUsers(idUser: number, idGuest: number): Promise<FriendList> {
         return await database.oneOrNone('select * from friend_list where id_user = $1 and id_guest = $2', [idUser, idGuest]);
+    }
+    async findByUser(idUser: number): Promise<User[]> {
+        return await database.manyOrNone('select u.id, u.name from friend_list f, user_account u where (f.id_user = $1 or f.id_guest = $1) and ((f.id_user = $1 && f.id_user = u.id) or (f.id_guest = $1 && f.id_guest = u.id))', [idUser]);
     }
     async areFriends(idUser: number, idGuest: number): Promise<FriendList> {
         return await database.oneOrNone('select * from friend_list where (id_user = $1 and id_guest = $2 or id_guest = $1 and id_user = $2) ', [idUser, idGuest, idGuest, idUser]);
