896fdcdfacd0d6f641a3d3418192ba27fcaa850a
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 01:43:37 -0300

    fix: findByUser friendlist repo

--- infra/friend-list-repository.ts
+++ infra/friend-list-repository.ts
@@ -11,7 +11,7 @@ export class FriendListRepositoryPostgres implements FriendListRepository {
         return await database.oneOrNone('select * from friend_list where id_user = $1 and id_guest = $2', [idUser, idGuest]);
     }
     async findByUser(idUser: number): Promise<User[]> {
-        return await database.manyOrNone('select distinct u.id, u.name from friend_list f, user_account u where (f.id_user = $1 or f.id_guest = $1)  and (f.id_user = u.id or f.id_guest = u.id)', [idUser]);
+        return await database.manyOrNone('select distinct u.id, u.name from friend_list f, user_account u where (f.id_user = $1 or f.id_guest = $1)  and (f.id_user = u.id or f.id_guest = u.id) and u.id <> $1', [idUser]);
     }
     async areFriends(idUser: number, idGuest: number): Promise<FriendList> {
         return await database.oneOrNone('select * from friend_list where (id_user = $1 and id_guest = $2 or id_guest = $1 and id_user = $2) ', [idUser, idGuest, idGuest, idUser]);
