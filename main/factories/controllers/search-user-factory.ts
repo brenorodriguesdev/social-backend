2518c492feb009526959e1e1a78571b4246ff3e4
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 22:15:00 -0300

    fix: search user factory

--- main/factories/controllers/search-user-factory.ts
+++ main/factories/controllers/search-user-factory.ts
@@ -1,10 +1,12 @@
 import { SearchUserService } from "../../../data/services/search-user";
+import { FriendListRepositoryPostgres } from "../../../infra/friend-list-repository";
 import { UserRepositoryPostgres } from "../../../infra/user-repository";
 import { SearchUserController } from "../../../presentation/controllers/search-user";
 import { makeSearchUserValidation } from "../validations/search-user-validation-factory";
 
 export const makeSearchUserController = (): SearchUserController => {
     const userRepositoryPostgres = new UserRepositoryPostgres()
-    const searchUserService = new SearchUserService(userRepositoryPostgres)
+    const friendListRepositoryPostgres = new FriendListRepositoryPostgres()
+    const searchUserService = new SearchUserService(userRepositoryPostgres, friendListRepositoryPostgres)
     return new SearchUserController(makeSearchUserValidation(), searchUserService)
 }
