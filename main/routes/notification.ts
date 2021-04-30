53bb04f46fb7990c5e5fe10f0029026f74c16239
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 21:44:55 -0300

    feat: add view notification route

--- main/routes/notification.ts
+++ main/routes/notification.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { adaptRouter } from "../adapters/express-controller-adapter";
 import { makeGetCountNotificationController } from "../factories/controllers/get-count-notification-controller-factory";
+import { makeViewNotificationController } from "../factories/controllers/view-notification-controller-factory";
 import { auth } from "../middlewares/auth";
 
 
 export default (router: Router): void => {
+    router.put('/viewNotification', auth, adaptRouter(makeViewNotificationController()))
     router.get('/getCountNotification', auth, adaptRouter(makeGetCountNotificationController()))
 }
