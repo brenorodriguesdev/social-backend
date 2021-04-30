efd5a1d500f96c870a58a2c2090d5c68d2fc0e64
brenorodriguesdev <brenorodriguesdev@gmail.com>
2021-04-29 21:09:50 -0300

    feat: add dynamic var on icon alert tab component

--- data/contracts/invite-repository.ts
+++ data/contracts/invite-repository.ts
@@ -5,5 +5,6 @@ export interface InviteRepository {
     findById(id: number): Promise<Invite>
     deleteById(id: number): Promise<void>
     findByUsers(idUser: number, idGuest: number): Promise<Invite>
+    findByGuest(idUser: number): Promise<Invite[]>
     count(idUser: number): Promise<number>
 }
